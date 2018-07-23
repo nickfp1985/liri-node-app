77be74c54f0c6d3926af9b4acb561160a69703e5
Nick Peddie <nfpeddie@gmail.com>
2018-07-23 11:41:49 -0700

    create main.js
    forgot to update my complete code from local repo

---
+++ main.js
@@ -0,0 +1,95 @@
+const app = require('./liri');
+const inquirer = require('inquirer');
+
+app.liri.connected(); // checks liri connection
+
+runLiri();
+
+function runLiri() {
+    // read the liri command and activate the corresponding functions
+    inquirer.prompt([
+        {
+            message: "Choose a Liri command:",
+            type: "list",
+            name: "command",
+            choices: ['get-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says']
+        }
+    ]).then(function (answers) {
+        let command = answers.command; // user input
+        switch (command) {
+            case 'get-tweets':
+                twitterReq();
+                break;
+            case 'spotify-this-song':
+                spotifyReq();
+                break;
+            case 'movie-this':
+                omdbReq();
+                break;
+            case 'do-what-it-says':
+                app.liri.mainSwitch('do-what-it-says', '-');
+                break;
+            default:
+        }
+    });
+}
+
+// **************************************************************************
+
+function omdbReq() {
+    inquirer.prompt([
+        {
+            message: "Movie:",
+            type: "input",
+            name: "movie"
+        }
+    ]).then(function (answer) {
+        if (answer.movie === undefined || answer.movie === null || answer.movie.length === 0) {
+            answer.movie = 'jaws';  // with no input, it will default to jaws imdb
+        }
+        app.liri.mainSwitch('movie-this', answer.movie);
+    })
+}
+
+function spotifyReq() {
+    inquirer.prompt([
+        {
+            message: "Song:",
+            type: "input",
+            name: "track"
+        }
+    ]).then(function (answer) {
+        if (answer.track === undefined || answer.track === null || answer.track.length === 0) {
+            answer.track = 'rock + roll'; // with no input, it will default to this song by EDEN
+        }
+        app.liri.mainSwitch('spotify-this-song', answer.track);
+    })
+}
+
+function twitterReq() {
+    inquirer.prompt([
+        {
+            message: "Twitter Name:",
+            type: "input",
+            name: "twitterScreenName"
+        }
+    ]).then(function (answer) {
+        if (answer.twitterScreenName === undefined || answer.twitterScreenName === null || answer.twitterScreenName.length === 0) {
+            answer.twitterScreenName = 'Twitter'; // if nothing is entered, default to this account
+        }
+        app.liri.twitterScreenName = answer.twitterScreenName;
+        app.liri.mainSwitch('get-tweets', '-');
+    })
+}
+
+module.exports.promptUser = function () {
+    inquirer.prompt([
+        {
+            message: "Try another Liri command?",
+            name: "restart",
+            type: "confirm"
+        }
+    ]).then(function (answer) {
+        (answer.restart) ? runLiri() : console.log('GOODBYE!');
+    })
+}
